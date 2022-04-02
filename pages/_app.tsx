0d37a43f10cb238c5ef914d34660017deef65d59
Alexander <lazarev-31071999@mail.ru>
2022-04-02 16:30:12 +0300

    Move to preact & add gql client (urql)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { createClient, Provider as UrqlProvider } from '@urql/preact';
+
+const client = createClient({
+  url: 'http://localhost:3000/api/gql',
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UrqlProvider value={client}>
+      <Component {...pageProps} />
+    </UrqlProvider>
+  );
 }
 
 export default MyApp;

8b4abdcf569eba477f0a664b24ed463fe8b6f31b
Alexander <madfox@laserflare.net>
2022-04-03 13:03:22 +0300

    Improve register form

--- pages/register/register.tsx
+++ pages/register/register.tsx
@@ -1,92 +1,113 @@
 import styles from './register.module.css';
-import {
-  TextInput,
-  Checkbox,
-  Button,
-  Group,
-  Box,
-  NumberInput,
-  Center,
-} from '@mantine/core';
+import { TextInput, Button, Group, Box, Center } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import 'dayjs/locale/ru';
 import Link from 'next/link';
 
-function Register() {
+export default function Register() {
   const form = useForm<{
-    name: string;
-    age: number | undefined;
+    username: string;
+    birthday: Date | null;
     email: string;
     password: string;
     confirmPassword: string;
   }>({
     initialValues: {
-      name: '',
-      age: undefined,
+      username: '',
+      birthday: null,
       email: '',
       password: '',
       confirmPassword: '',
     },
 
     validate: (values) => ({
-      name: 
-            values.name.length < 2 || values.name.length > 16
-              ? 'Имя пользователя должно состоять из 2-16 символов '
-              : !/^[A-z0-9_]/.test(values.name) 
-              ? 'Имя пользователя может содержать только буквы, цифры и символы подчеркивания'
-              : null,
+      username:
+        values.username.length < 2 || values.username.length > 16
+          ? 'Имя пользователя должно состоять из 2-16 символов '
+          : !/^[A-z0-9_]/.test(values.username)
+          ? 'Имя пользователя может содержать только буквы, цифры и нижнюю черту'
+          : null,
 
-
-      email: !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email) ? 'Неверная почта' : null ,
-      password: 
-            values.password.length < 8 || values.password.length > 32
-              ? 'Пароль должен состоять из 8-32 символов'
-              : !/^[A-Za-z\d@$!%*?&#]$/.test(values.password) 
-              ? 'Пароль должен содержать только буквы, цифры и специальные символы'
-              : null,
+      email: !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)
+        ? 'Неверная почта'
+        : null,
+      password:
+        values.password.length < 8 || values.password.length > 32
+          ? 'Пароль должен состоять из 8-32 символов'
+          : !/^[A-Za-z\d@$!%*?&#]{8,32}$/.test(values.password)
+          ? 'Пароль должен содержать только буквы, цифры и специальные символы'
+          : null,
 
       confirmPassword:
         values.confirmPassword != values.password
           ? 'Пароли не совпадает'
           : null,
-      age: values.age === undefined ? 'Age is required' : null,
     }),
   });
+
+  const handleSubmit = (values: typeof form.values) => {
+    console.dir(values);
+  };
+
   return (
     <div className="container">
       <Center className={styles.form_block}>
         <Box className={styles.form_container}>
-          <h1 className={styles.title}>Регистрация</h1>
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <h1 className={styles.title}>Регистрация</h1>
             <TextInput
               label="Почта"
+              type="email"
+              name="email"
               placeholder="your@email.com"
+              disabled={registerResult.fetching}
               {...form.getInputProps('email')}
             />
             <TextInput
               label="Имя пользователя"
+              type="text"
+              name="username"
               placeholder="Иван"
-              {...form.getInputProps('name')}
+              disabled={registerResult.fetching}
+              {...form.getInputProps('username')}
             />
             <DatePicker
               locale="ru"
+              allowFreeInput
+              value={birthday}
+              name="dob"
+              onChange={setBirthday}
               placeholder="Выберите дату"
+              disabled={registerResult.fetching}
               label="Дата рождения"
             />
+            <small style={{ opacity: 0.5 }}>
+              Эта информация будет использована для адаптации сложности
+            </small>
             <TextInput
               label="Пароль"
-              placeholder="Qwerty1234."
+              name="password"
+              type="password"
+              placeholder="Qwerty1234"
+              disabled={registerResult.fetching}
               {...form.getInputProps('password')}
             />
             <TextInput
               label="Подтвердите пароль"
-              placeholder="Qwerty1234."
+              name="passwordConfirm"
+              type="password"
+              placeholder="Qwerty1234"
+              disabled={registerResult.fetching}
               {...form.getInputProps('confirmPassword')}
             />
 
             <Group position="center" direction="column" spacing="xs" mt="md">
-              <Button type="submit">Зарегистрироваться</Button>
+              <Button type="submit" disabled={registerResult.fetching}>
+                {registerResult.fetching
+                  ? 'Подождите...'
+                  : 'Зарегистрироваться'}
+              </Button>
               <Link href="/signin">
                 <a className={styles.navbar__element}>Вернуться</a>
               </Link>
@@ -97,5 +118,3 @@ function Register() {
     </div>
   );
 }
-
-export default Register;

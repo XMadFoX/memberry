7f8afb187e58f3635f941f93f14e58e17b20542f
Alexander <madfox@laserflare.net>
2022-04-03 13:34:28 +0300

    Improve login form

--- pages/signin/signin.tsx
+++ pages/signin/signin.tsx
@@ -2,45 +2,26 @@ import { TextInput, Button, Group, Box, Center } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import 'dayjs/locale/ru';
 import style from './signin.module.css';
-import Link from 'next/link'; 
+import Link from 'next/link';
 
 function Register() {
   const form = useForm<{
-    name: string;
-    age: number | undefined;
     email: string;
     password: string;
-    confirmPassword: string;
   }>({
     initialValues: {
-      name: '',
-      age: undefined,
       email: '',
       password: '',
-      confirmPassword: '',
     },
 
     validate: (values) => ({
-      name: 
-            values.name.length < 2 || values.name.length > 16
-              ? 'Имя пользователя должно состоять из 2-16 символов '
-              : !/^[A-z0-9_]/.test(values.name) 
-              ? 'Имя пользователя может содержать только буквы, цифры и символы подчеркивания'
-              : null,
-
-
-      email: !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email) ? 'Неверная почта' : null ,
-      password: 
-            values.password.length < 8 || values.password.length > 32
-              ? 'Пароль должен состоять из 8-32 символов'
-              : !/^[A-Za-z\d@$!%*?&#]$/.test(values.password) 
-              ? 'Пароль должен содержать только буквы, цифры и специальные символы'
-              : null,
-      confirmPassword:
-        values.confirmPassword != values.password
-          ? 'Пароли не совпадает'
+      email: !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)
+        ? 'Неверная почта'
+        : null,
+      password:
+        values.password.length < 8 || values.password.length > 32
+          ? 'Пароль должен состоять из 8-32 символов'
           : null,
-      age: values.age === undefined ? 'Age is required' : null,
     }),
   });
   return (
@@ -53,11 +34,15 @@ function Register() {
             <h1 className={style.title}>Вход</h1>
             <TextInput
               label="Почта"
+              type="email"
+              name="email"
               placeholder="your@email.com"
               {...form.getInputProps('email')}
             />
             <TextInput
               label="Пароль"
+              type="password"
+              name="password"
               placeholder="Qwerty1234."
               {...form.getInputProps('password')}
             />
@@ -65,9 +50,9 @@ function Register() {
               <Button type="submit">Войти</Button>
             </Group>
             <nav className={style.navbar}>
-              <Link href="/signin/recovery">
+              {/* <Link href="/signin/recovery">
                 <a className={style.navbar__element}>Забыли пароль?</a>
-              </Link>
+              </Link> */}
               <Link href="/register">
                 <a className={style.navbar__element}>Зарегистрироваться</a>
               </Link>

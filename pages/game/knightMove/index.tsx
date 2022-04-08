f8a41145e6cee92b4f3e8a3507fb915269bf0c9c
Marsen <marsenzakirov11@gmail.com>
2022-04-08 13:24:23 +0300

    Co-authored-by: MadFox <XMadFoX@users.noreply.github.com>

--- pages/game/knightMove/index.tsx
+++ pages/game/knightMove/index.tsx
@@ -1,2 +1,11 @@
-import KnightMove from "./knightMove";
-export default KnightMove
+import React from 'react';
+import GameBlock from '@components/gameBlock/gameBlock';
+import KnightMove from './knightMove';
+
+export default function Index() {
+  return (
+    <GameBlock>
+      <KnightMove />
+    </GameBlock>
+  );
+}

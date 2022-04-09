5fa73de9292d5e09d7417cc3c51d4e60ce9a1b40
Marsen <marsenzakirov11@gmail.com>
2022-04-09 15:29:32 +0300

    Co-authored-by: MadFox <XMadFoX@users.noreply.github.com>

--- pages/game/remember-squares.tsx
+++ pages/game/remember-squares.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function RememberSquares() {
   return (
-    <GameBlock>
+    <GameBlock id = "remember-squares">
       <FillingSquares />
     </GameBlock>
   );
